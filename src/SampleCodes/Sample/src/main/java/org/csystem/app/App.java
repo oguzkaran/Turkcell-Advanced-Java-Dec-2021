0a953d9e3cafc0984efc241ad4c5b07dd89a9266
Oğuz Karan <oguzkaran@csystem.org>
2021-12-27 09:34:47 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
@@ -1,6 +1,5 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Thread sınıfının static isInterrupted metodu çağrıldığı thread işterrupt flag değeri set edilmişse true, edilmemişse
-    false değerine geri döner. Aşaıdaki örneği inceleyiniz
+    TODO:
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
@@ -12,13 +11,20 @@ class App {
     {
         Runnable runnable = () -> {
             var self = Thread.currentThread();
+            for (int i = 0; !Thread.interrupted(); ++i)
+                Console.write("First Loop:%d%n", i);
+
             for (int i = 0; !self.isInterrupted(); ++i)
-                Console.write("%d ", i);
+                Console.write("Second Loop:%d%n", i);
+
+            Console.writeLine("Thread ends!...");
         };
 
         var t = new Thread(runnable);
 
         t.start();
+        ThreadUtil.sleep(3000);
+        t.interrupt();
         ThreadUtil.sleep(3000);
         t.interrupt();
     }
