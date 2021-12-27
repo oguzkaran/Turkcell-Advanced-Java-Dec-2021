3d96e1fa3f0ce445078acc32c9f80660af108b47
Oğuz Karan <oguzkaran@csystem.org>
2021-12-27 12:00:26 +0300

    Update

---
+++ src/SampleCodes/Sample/src/main/java/com/turkcell/app/random/RandomGeneratorTotal.java
@@ -0,0 +1,79 @@
+package com.turkcell.app.random;
+
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomGeneratorTotal {
+    private long m_total;
+    private int m_count;
+    private final int m_stopValue;
+    private final int m_min;
+    private final int m_max;
+    private final int m_maxCount;
+    private final int m_threadCount;
+    private final ArrayList<Thread> m_threads;
+
+    private void createThreads()
+    {
+        for (var i = 0; i < m_threadCount; ++i) {
+            var t = new Thread(this::threadCallback);
+
+            t.start();
+            m_threads.add(t);
+        }
+    }
+
+    public void joinThreads()
+    {
+        try {
+            for (var thread : m_threads)
+                thread.join();
+        }
+        catch (InterruptedException ignore) {
+
+        }
+    }
+
+    private void threadCallback()
+    {
+        var random = ThreadLocalRandom.current();
+
+        for (var i = 0; i < m_maxCount; ++i) {
+            synchronized (this) {
+                var val = random.nextInt(m_min, m_max + 1);
+
+                if (val == m_stopValue)
+                    break;
+
+                m_total += val;
+                ++m_count;
+            }
+        }
+    }
+
+    public RandomGeneratorTotal(int stopValue, int min, int max, int maxCount, int threadCount)
+    {
+        m_stopValue = stopValue;
+        m_min = min;
+        m_max = max;
+        m_maxCount = maxCount;
+        m_threadCount = threadCount;
+        m_threads = new ArrayList<>();
+    }
+
+    public long getTotal()
+    {
+        return m_total;
+    }
+
+    public int getCount()
+    {
+        return m_count;
+    }
+
+    public void run()
+    {
+        createThreads();
+        joinThreads();
+    }
+}
--- src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
@@ -1,83 +1,19 @@
 /*----------------------------------------------------------------------------------------------------------------------
-     Bazı durumlarda synchronized deyimi için bir nesne yokda Object türünden nesne yaratılabilir
+     Aşağdıdaki örnekte Collections sınıfının synchronizedList metodu kullanılarak senkronizasyon problemi çözülmüştür
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
+import com.turkcell.app.random.RandomGenerator;
 import org.csystem.util.console.Console;
-
-import java.util.ArrayList;
 
 class App {
     public static void main(String[] args)
     {
-        var id = new IncrementerDecrementer(3, 10_000_000);
+        var rg = new RandomGenerator(1_000_000, 10, 20, 5);
 
-        id.run();
+        rg.run();
 
-        Console.writeLine("val = %d", id.getVal());
-    }
-}
-
-class IncrementerDecrementer {
-    private int m_val;
-    private final int m_threadCount;
-    private final long m_count;
-    private final ArrayList<Thread> m_threads;
-
-    private void createThreads(Runnable runnable)
-    {
-        for (var i = 0; i < m_threadCount; ++i) {
-            var t = new Thread(runnable);
-
-            t.start();
-            m_threads.add(t);
-        }
-    }
-
-    private void incrementThreadCallback()
-    {
-        for (var i = 0L; i < m_count; ++i)
-            synchronized (this) {
-                ++m_val;
-            }
-    }
-
-    private void decrementThreadCallback()
-    {
-        for (var i = 0L; i < m_count; ++i)
-            synchronized (this) {
-                --m_val;
-            }
-    }
-
-    private void joinThreads()
-    {
-        try {
-            for (var thread : m_threads)
-                thread.join();
-        }
-        catch (InterruptedException ignore) {
-
-        }
-    }
-
-    public IncrementerDecrementer(int threadCount, long count)
-    {
-        m_threadCount = threadCount;
-        m_count = count;
-        m_threads = new ArrayList<>();
-    }
-
-    public int getVal()
-    {
-        return m_val;
-    }
-
-    public void run()
-    {
-        createThreads(this::incrementThreadCallback);
-        createThreads(this::decrementThreadCallback);
-        joinThreads();
+        Console.writeLine("Size:%d", rg.getPasswords().size());
     }
 }
 
